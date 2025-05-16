dbd9b0a5debecd8aa15e0815940828d65b1dd772
Pragathi <pragathi.is22@bmsce.ac.in>
2025-05-17 01:38:40 +0530

    Last

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import connectWithMongoDB from './db/Connection1.js'
 import cors from 'cors'
 
 app.use(cors({
-    origin: ["http://localhost:3000","https://spontaneous-kleicha-15566b.netlify.app/"]
+    origin: ["http://localhost:3000","https://cloud-sy8o.vercel.app/"]
 }));
 
 connectWithMongoDB();
